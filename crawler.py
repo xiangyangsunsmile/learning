483fb1738b76e3cf6975192ce27fadeb5e018b50
xiangyang <heyanglv@126.com>
2015-08-13 18:57:11 +0800

    a crawler learning

--- crawler.py
+++ crawler.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 '''
 author: yang
-
 date:   2015/8/13
-
 description:   爬虫
-
 params:
-    url_queue:存放尚未爬取的url的队列
-    url_set ：存放已爬取的url的集合
-
-    url_name ：网站根目录
-    short_html：html正则表达式
-    short_css：css正则表达式
-    short_js：js正则表达式
-    short_jpg：jpg正则表达式
-    short_png: png正则表达式
-
-    NameList = []:存放可爬取的url的列表
+    url_queue:       存放尚未爬取的url的队列
+    url_set ：       存放已爬取的url的集合
+    url_name ：      网站根目录
+    short_html：     html正则表达式
+    short_css：      css正则表达式
+    short_js：       js正则表达式
+    short_jpg：      jpg正则表达式
+    short_png:       png正则表达式
+    NameList = []:   存放可爬取的url的列表
     WrongNameList = []：存放产生异常的url的列表
-
 '''
 import Queue
 import urllib2
@@ -29,7 +23,7 @@ import time
 import string
 import os
 import pdb
-url_queue = Queue.Queue(0)#队列
+url_queue = Queue.Queue(0)
 url_set = set()
 url_name = "http://www.xxxx.edu.cn/"
 short_html = ".*xxxx.*"
@@ -46,10 +40,7 @@ NameList = []
 WrongNameList = []
 class url_get:
     def __init__(self):
-        #threading.Thread.__init__(self)
-        #self.urlname = "www.xxxx.edu.cn"#存放加入的根目录，pdf是网站的名字
         url_queue.put("http://www.xxxx.edu.cn",0.5);
-        #url_set.add("http://www.pdf.com")
         i = 0
     def run(self):
         cas = 0
@@ -59,18 +50,16 @@ class url_get:
             print "j=="+str(j), "len of queue:",url_queue.qsize()
             if url_queue.empty() == True :
                 print u"finish"
-                #time.sleep(20)
-                #self.run()
             else :
-                self.recent_url = url_queue.get(0.5)#操作的url
-                if self.recent_url not in url_set :#该url还未被爬取过
-                    url_set.add(self.recent_url)#在爬取之前先添加到以集合url_set里
+                self.recent_url = url_queue.get(0.5)    #操作的url
+                if self.recent_url not in url_set :     #该url还未被爬取过
+                    url_set.add(self.recent_url)        #在爬取之前先添加到以集合url_set里
                     try :
-                        url_need = urllib2.urlopen(self.recent_url).read()#爬取网页
+                        url_need = urllib2.urlopen(self.recent_url).read()  #爬取网页
                     except :
-                        WrongNameList.append(self.recent_url)#打不开的时候进行处理，防止程序不正常结束
-                    url_list = re.findall('"((http|ftp)s?://.*?)"', url_need)#正则表达式，找出网页里面的所有链接
-                    #url_list_second = re.findall('<a.*?href="([^"]*)".*?>([\S\s]*?)</a>', url_need) 这是我第二次爬的时候用的正则表达式
+                        WrongNameList.append(self.recent_url)   #打不开的时候进行处理，防止程序不正常结束
+                    url_list = re.findall('"((http|ftp)s?://.*?)"', url_need)   #正则表达式，找出网页里面的所有链接
+                    #url_list_second = re.findall('<a.*?href="([^"]*)".*?>([\S\s]*?)</a>', url_need) 
                     if re.match(css_short,self.recent_url) :
                         sName = string.zfill(cas,4)+'.css'
                     elif re.match(js_short,self.recent_url) :
@@ -78,11 +67,11 @@ class url_get:
                     elif re.match(jpg_short,self.recent_url) :
                         continue
                     elif re.match(png_short,self.recent_url) :
-                        continue #图片都略过了，因为下载下来的都不完整
+                        continue 
                     else :
                         sName = string.zfill(cas,4)+'.html'
                     url_text = open(sName,"w+")
-                    url_text.write(url_need)#爬去recent_url网页的内容写到文件里面
+                    url_text.write(url_need)    #爬取recent_url网页的内容写到文件里面
                     url_text.close()
                     NameList.append(sName + self.recent_url)
                     n=0
